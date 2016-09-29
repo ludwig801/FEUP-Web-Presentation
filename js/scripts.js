fcf867db0325a53ac4e50acc1a9fb03c6d1abd9b
Luís Magalhães <lmaga801@outlook.com>
2016-09-29 15:55:44 +0100

    Improved website legibility

--- js/scripts.js
+++ js/scripts.js
@@ -24,24 +24,29 @@ function CollapseBehavior() {
     var content = $(".content");
     var collapsables = content.find(".collapsable");
 
-    console.log(collapsables);
-
     $(collapsables).each(function(index, item) {
         var jQueryItem = $(item);
 
         var toggle = $(jQueryItem.find(".collapse-toggle"));
         var target = $(jQueryItem.find(".collapse-target"));
+        var label = $(toggle.find(".toggle-label"));
         
         toggle.removeClass("visible");
         toggle.addClass("");
         target.hide();
+
+        label.addClass("arrow-down");
         
         toggle.on("click", function() {
             if(toggle.hasClass("visible")) {
                 target.hide(animDuration);
+                label.addClass("arrow-down");
+                label.removeClass("arrow-up");
             }
             else {
                 target.show(animDuration);
+                label.addClass("arrow-up");
+                label.removeClass("arrow-down");
             }
             toggle.toggleClass("visible");
         });
